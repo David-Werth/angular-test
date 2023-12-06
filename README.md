88588ac21dd7bad73da3e03eddb597b64405c132
David-Werth <75678655+David-Werth@users.noreply.github.com>
2023-12-06 10:11:54 +0100

    final commit

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
-# Angular Homes App
+# Angular Test
+
 - Install Angular if you don't have it installed
 
   `npm install -g @angular/cli`
 
 - Clone this branch to your local machine
 
-  `git clone -b homes-app-start git@github.com:angular/codelabs.git homes-app`
+  `git clone https://github.com/David-Werth/angular-test.git`
 
 - Once the code has been downloaded
 
-  `cd homes-app`
+  `cd angular-test`
 
 - Install the depencies
 
-  `npm install` 
+  `npm install`
 
-- Run the application 
+- Run the json server
+
+  `json-server --watch db.json`
+
+- Run the application
 
   `ng serve`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
-  styleUrls: ['./app.component.css'],
+	standalone: true,
+	selector: 'app-root',
+	imports: [HomeComponent, RouterModule],
+	template: `<main>
+		<header class="brand-name">
+			<img
+				src="/assets/logo.svg"
+				alt="brand-logo"
+				class="brand-logo"
+				aria-hidden="true"
+			/>
+		</header>
+		<section class="content">
+			<router-outlet></router-outlet>
+		</section>
+	</main>`,
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+	title = 'homes';
 }
